a82c33bba769c5104120c27b7d2bd9bc0e3ceced
bentlynix <bentlynixon20@gmail.com>
2019-12-02 22:44:05 +0530

    Fix ngapp issue && add check box

--- script.js
+++ script.js
@@ -46,3 +46,26 @@ $scope.dataTableOpt = {
   "aLengthMenu": [[25, 50, 100,-1], [25, 50, 100,'All']],
   };
 });
+
+app.controller('ModalDemoCtrl', function ($scope, $uibModal) {
+
+  $scope.open = function () {    
+    $uibModal.open({
+      animation: true,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: 'lg'
+    });
+}
+  
+})
+app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance) {
+
+  $scope.ok = function () {
+    $uibModalInstance.close();
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
